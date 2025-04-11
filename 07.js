f7e6666d74daec467e136022527478ca91de1395
kashyap2207 <kashyap.dhamecha.cg@gmail.com>
2025-04-11 10:47:04 +0530

    que 7 completed

---
+++ 07.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+const users = [
+    { id: 1, name: "Alice", email: "alice@example.com" },
+    { id: 2, name: "John", email: "john@example.com" },
+    { id: 3, name: "marie", email: "marie@example.com" }
+];
+
+app.get('/', (req, res) => {
+    res.send('Server is running')
+});
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+app.get('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const userId = parseInt(id);
+    const user = users.find(u => u.id === userId);
+
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).json({ message: 'User not found' });
+    }
+});
+
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.status(201).json(newUser);
+});
+
+app.patch('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const userId = parseInt(id);
+    const updates = req.body;
+
+    const user = users.find(u => u.id === userId);
+
+    if (user) {
+        Object.assign(user, updates);
+        res.status(200).json(user);
+    } else {
+        res.status(404).json({ message: 'User not found' });
+    }
+});
+
+app.delete('users/:id', (req, res) => {
+    const { id } = req.params;
+    const userId = parseInt(id);
+
+    const userExists = users.some(u => u.id === userId);
+
+    if (userExists) {
+        users = users.filter(u => u.id !== userId);
+        res.sendStatus(204);
+    } else {
+        res.sendStatus(404).json({ message: 'User not found' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
